cce0284710f4aec978cbc892753f2c10361441a6
Peco Danajlovski <vortexmk@gmail.com>
2011-09-09 13:12:06 +0200

    implemented fetching of basic info for a band

--- lib/metallum/version.rb
+++ lib/metallum/version.rb
@@ -1,3 +1,3 @@
 module Metallum
-  VERSION = "0.0.1"
+  VERSION = "0.1.0"
 end
--- spec/metallum/metallum_spec.rb
+++ spec/metallum/metallum_spec.rb
@@ -4,18 +4,57 @@ require 'spec_helper'
 
 describe Metallum do
 
+  before(:each) do
+    @band_url = 'http://www.metal-archives.com/bands/Blind_Guardian/3'
+    @agent = Metallum::Agent.new(@band_url)
+    @band = @agent.fetch_band
+  end
+
   it "should fetch a band's name from url for band 'Hell'" do
-    band_url = 'http://www.metal-archives.com/bands/Hell/5849'
-    @agent = Metallum::Agent.new(band_url)
+    @band_url = 'http://www.metal-archives.com/bands/Hell/5849'
+    @agent = Metallum::Agent.new(@band_url)
     band = @agent.fetch_band
     band.name.should == "Hell"
   end
   
-  it "should fetch a band's name from url for band 'Blind Guardian'" do
-    band_url = 'http://www.metal-archives.com/bands/Blind_Guardian/3'
-    @agent = Metallum::Agent.new(band_url)
-    band = @agent.fetch_band
-    band.name.should == "Blind Guardian"
+  it "should fetch the band's name from url for band 'Blind Guardian'" do
+    @band.name.should == "Blind Guardian"
+  end
+
+  it "should fetch the band's country of origin" do
+    @band.country.should == "Germany"
+  end
+
+  it "should fetch the band's location" do
+    @band.location.should == "Krefeld, North Rhine-Westphalia"
+  end
+
+  it "should fetch band's status" do
+    @band.status.should == "Active"
+  end
+
+  it "should fetch the band's year of creation" do
+    @band.year_formed.should == "1986"
+  end
+
+  it "should fetch the band's genre" do
+    @band.genre.should == "Speed Metal (early), Power Metal (Later)"
+  end
+
+  it "should fetch band's lyrical themes" do
+    @band.lyrical_themes.should == "Epic Tales, Legends, Fantasy, Tolkien, Literature"
+  end
+
+  it "should fetch band's record label" do
+    @band.record_label.should == "Nuclear Blast Records"
+  end
+
+  it "should fetch band's photo url" do
+    @band.photo_url.should == "http://www.metal-archives.com/images/3/3_photo.jpg"
+  end
+
+  it "should fetch band's logo url" do
+    @band.logo_url.should == "http://www.metal-archives.com/images/3/3_logo.gif"
   end
 
 end
